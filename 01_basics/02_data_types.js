e5edeb185d8bc89116bec988ac5e34eed19ee1d3
virajmandlik <138781016+virajmandlik@users.noreply.github.com>
2024-06-20 08:00:14 +0000

    First importatn thing done

---
+++ 01_basics/02_data_types.js
@@ -0,0 +1,10 @@
+"use strict" //strictly follow newer js version
+
+// alert(3 +3)  // we ae using the node js nand not the browsers
+
+console.log('hello')
+
+let a= 45
+let b = "hello"
+console.table([typeof(a),typeof(b)])
+console.table([typeof(null),typeof(undefined)])
